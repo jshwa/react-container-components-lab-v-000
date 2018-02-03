b4a0b4d96d54e02f33570892e09dcdd682573c81
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-03 20:16:44 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = '';
+const NYT_API_KEY = 'd5dad6d196ce48d0adc1f3a955a58810';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
